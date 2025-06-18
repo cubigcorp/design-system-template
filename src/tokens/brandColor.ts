21b62ad7b098c9bb4d644fd29b7dcc3854a28fc4
inhwa <inhwa@cubig.ai>
2025-06-18 16:58:09 +0900

    fix: 브랜드 컬러 수정

--- src/tokens/brandColor.ts
+++ src/tokens/brandColor.ts
@@ -2,12 +2,14 @@ import color from "./color";
 
 const brandColor = {
   light: {
-    "fg-brand-primary": color.deeppurple["800"],
+    "fg-brand-primary": color.deeppurple["500"],
+    "fg-brand-strong": color.deeppurple["800"],
     "bg-brand-primary-default": color.deeppurple["800"],
     "bg-brand-primary-pressed": color.deeppurple["900"],
   },
   dark: {
-    "fg-brand-primary": color.deeppurple["800"],
+    "fg-brand-primary": color.deeppurple["500"],
+    "fg-brand-strong": color.deeppurple["50"],
     "bg-brand-primary-default": color.deeppurple["800"],
     "bg-brand-primary-pressed": color.deeppurple["700"],
   },

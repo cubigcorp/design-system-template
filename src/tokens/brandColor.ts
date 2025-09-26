c3f5a4ca26156fdea3effb06afa98193ec0d3293
정인화 <jung-inhwa@MacBook-Air-3.local>
2025-09-26 13:56:10 +0900

    fix: brand color수정

--- src/tokens/brandColor.ts
+++ src/tokens/brandColor.ts
@@ -2,16 +2,16 @@ import color from "./color";
 
 const brandColor = {
   light: {
-    "fg-brand-primary": color.deeppurple["500"],
-    "fg-brand-strong": color.deeppurple["800"],
-    "bg-brand-primary-default": color.deeppurple["800"],
-    "bg-brand-primary-pressed": color.deeppurple["900"],
+    "fg-brand-primary": color.purple["500"],
+    "fg-brand-strong": color.purple["800"],
+    "bg-brand-primary-default": color.purple["800"],
+    "bg-brand-primary-pressed": color.purple["900"],
   },
   dark: {
-    "fg-brand-primary": color.deeppurple["500"],
-    "fg-brand-strong": color.deeppurple["50"],
-    "bg-brand-primary-default": color.deeppurple["800"],
-    "bg-brand-primary-pressed": color.deeppurple["700"],
+    "fg-brand-primary": color.purple["500"],
+    "fg-brand-strong": color.purple["50"],
+    "bg-brand-primary-default": color.purple["800"],
+    "bg-brand-primary-pressed": color.purple["700"],
   },
 } as const;
 

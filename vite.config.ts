8f8d619bd46c45c7bc3e928896102bcc09a1eb6e
정인화 <jung-inhwa@MacBook-Air-4.local>
2025-10-21 15:55:33 +0900

    fix: vite 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     dts({
       insertTypesEntry: true,
-      include: ["src/components/**/*", "src/tokens/**/*", "src/index.ts"],
+      include: ["src/components/**/*", "src/tokens/**/*", "src/index.ts", "src/vite-env.d.ts"],
       exclude: [
         "src/app/**/*",
         "src/stories/**/*",

94d7c993bff40fd4a9692a6372b5cd6278e1ed46
정인화 <jung-inhwa@MacBook-Pro.local>
2025-12-16 15:13:07 +0900

    fix: checkbox 수정

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -60,22 +60,32 @@ const StyledCheckbox = styled.div<{
   padding: ${spacing.gap["gap-0"]};
 
   ${({ $variant, $state, $disabled }) => {
-    if ($disabled) {
-      return `
-        background-color: ${color.gray[50]};
-        border: 1px solid ${borderColor.light["color-border-primary"]};
-        color: ${textColor.light["fg-neutral-disable"]};
-      `;
-    }
-
     if ($variant === "primary") {
       if ($state === "checked") {
+        if ($disabled) {
+          // Primary + Checked + Disabled
+          return `
+            background-color: ${color.gray[700]};
+            border: 1px solid ${color.gray[600]};
+            color: ${color.gray[500]};
+          `;
+        }
+        // Primary + Checked
         return `
           background-color: ${color.gray[950]};
           border: 1px solid ${color.gray[925]};
           color: ${color.common[100]};
         `;
       } else {
+        if ($disabled) {
+          // Primary + Unchecked + Disabled
+          return `
+            background-color: ${color.gray[50]};
+            border: 1px solid ${borderColor.light["color-border-alternative"]};
+            color: ${textColor.light["fg-neutral-disable"]};
+          `;
+        }
+        // Primary + Unchecked
         return `
           background-color: ${color.common[100]};
           border: 1px solid ${color.gray[400]};
@@ -84,11 +94,38 @@ const StyledCheckbox = styled.div<{
       }
     }
 
-    return `
-      background-color: ${color.common[100]};
-      border: 1px solid ${color.gray[400]};
-      color: ${textColor.light["fg-neutral-strong"]};
-    `;
+    // Secondary
+    if ($state === "checked") {
+      if ($disabled) {
+        // Secondary + Checked + Disabled
+        return `
+          background-color: ${color.gray[50]};
+          border: 1px solid ${borderColor.light["color-border-primary"]};
+          color: ${textColor.light["fg-neutral-disable"]};
+        `;
+      }
+      // Secondary + Checked
+      return `
+        background-color: ${color.common[100]};
+        border: 1px solid ${color.gray[400]};
+        color: ${textColor.light["fg-neutral-strong"]};
+      `;
+    } else {
+      if ($disabled) {
+        // Secondary + Unchecked + Disabled
+        return `
+          background-color: ${color.gray[50]};
+          border: 1px solid ${borderColor.light["color-border-alternative"]};
+          color: ${textColor.light["fg-neutral-disable"]};
+        `;
+      }
+      // Secondary + Unchecked
+      return `
+        background-color: ${color.common[100]};
+        border: 1px solid ${color.gray[400]};
+        color: ${textColor.light["fg-neutral-strong"]};
+      `;
+    }
   }}
 
   &:disabled {
